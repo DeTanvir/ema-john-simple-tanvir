f78c9b824223186be0f2f2833c391eabda8b47d2
DeTanvir <tanvirahmed14.ta@gmail.com>
2023-03-30 12:08:53 +0600

    display grid for shop

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h2>This is our products.</h2>
+            </div>
+            <div className="cart-container">
+                <h2>Order Review</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
