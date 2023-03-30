5604c9faf6cab5c1bb95a3a936d51eff9e20ec17
DeTanvir <tanvirahmed14.ta@gmail.com>
2023-03-30 16:53:20 +0600

    after a break

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
@@ -14,7 +16,7 @@ const Product = (props) => {
                 <p>Rating: {ratings} Stars</p>
             </div>
             <button onClick={ () => handleAddToCart(props.product)} className="btn-cart">
-                Add to Cart
+                Add to Cart <FontAwesomeIcon icon={faCartPlus} />
             </button>
         </div>
     );
