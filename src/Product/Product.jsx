8fe2381e87f7b4c5828e7882d099dc58007c5091
DeTanvir <tanvirahmed14.ta@gmail.com>
2023-03-30 13:27:25 +0600

    product info

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product)
     const {name, price, seller, img, ratings} = props.product;
     return (
-        <div>
+        <div className='product'> 
             <img src= {img} alt="shoe-image" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <h6 className='product-price'>Price: ${price}</h6>
+                <p>Manufacturer : {seller}</p>
+                <p>Rating: {ratings} Stars</p>
+            </div>
         </div>
     );
 };
